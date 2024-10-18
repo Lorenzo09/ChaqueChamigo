c202ecad0f8c87bc4ced5b8641b5667fcb56ab46
Lorenzo Lacava <lorenzolacava1@gmail.com>
2024-10-18 18:06:34 -0300

    parq

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import osmnx as ox
 import networkx as nx
 
 # Cargar los datos de siniestros
-df = pd.read_parquet('Datasets_limpios/siniestrosfinal.parquet')
+df = pd.read_parquet('Dataset_limpios/siniestrosfinal.parquet')
 
 # Título del dashboard
 st.title("ChaqueChamigo - Dashboard de Siniestros Viales en Corrientes")
@@ -32,25 +32,27 @@ st.header("Mapa de calor de siniestros - Selecciona el punto de partida y destin
 map_data = st_folium(m, width=700, height=500)
 
 # Inicializar las variables de coordenadas seleccionadas
-start_coords = None
-end_coords = None
+if 'start_coords' not in st.session_state:
+    st.session_state['start_coords'] = None
+if 'end_coords' not in st.session_state:
+    st.session_state['end_coords'] = None
 
 # Verificar si el usuario ha hecho clic en el mapa
-if map_data and 'last_clicked' in map_data:
-    if not start_coords:
-        start_coords = (map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
-        st.write(f"Coordenadas de partida seleccionadas: {start_coords}")
-    elif not end_coords:
-        end_coords = (map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
-        st.write(f"Coordenadas de destino seleccionadas: {end_coords}")
+if map_data and map_data['last_clicked'] is not None:
+    if st.session_state['start_coords'] is None:
+        st.session_state['start_coords'] = (map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
+        st.write(f"Coordenadas de partida seleccionadas: {st.session_state['start_coords']}")
+    elif st.session_state['end_coords'] is None:
+        st.session_state['end_coords'] = (map_data['last_clicked']['lat'], map_data['last_clicked']['lng'])
+        st.write(f"Coordenadas de destino seleccionadas: {st.session_state['end_coords']}")
 
 # Generar el grafo de Corrientes usando OSMnx
-if start_coords and end_coords:
+if st.session_state['start_coords'] and st.session_state['end_coords']:
     G = ox.graph_from_place('Corrientes, Argentina', network_type='drive')
 
     # Obtener el nodo más cercano a las coordenadas seleccionadas
-    start_node = ox.distance.nearest_nodes(G, start_coords[1], start_coords[0])
-    end_node = ox.distance.nearest_nodes(G, end_coords[1], end_coords[0])
+    start_node = ox.distance.nearest_nodes(G, st.session_state['start_coords'][1], st.session_state['start_coords'][0])
+    end_node = ox.distance.nearest_nodes(G, st.session_state['end_coords'][1], st.session_state['end_coords'][0])
 
     # Calcular la ruta más corta
     route = nx.shortest_path(G, start_node, end_node, weight='length')
@@ -58,4 +60,5 @@ if start_coords and end_coords:
     # Mostrar la ruta en el mapa
     route_map = ox.plot_route_folium(G, route, route_map=m)
     st_folium(route_map, width=700, height=500)
+
 
